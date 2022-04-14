e4014e850d716153b900973a25ac785351496001
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-04-15 00:23:14 +0100

    Restructure the header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import NavBar from './components/NavBar';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <NavBar />
-      </header>
+      <Header />
       <Outlet />
     </div>
   );
