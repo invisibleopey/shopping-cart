64b5e29f9d47b72cf572d61598e589b475333ace
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-04-15 16:05:55 +0100

    Implement home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import picture from '../assets/picture.jpg';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="left">
+        <img src={picture} alt="five friends in street clothes" />
+        <span>
+          Photo by{' '}
+          <a href="https://unsplash.com/@forcemajeure?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Force Majeure
+          </a>{' '}
+          on{' '}
+          <a href="https://unsplash.com/s/photos/fashion-shoot?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Unsplash
+          </a>
+        </span>
+      </div>
+      <div className="right">
+        <span>Best online store of the year</span>
+        <h1>We are fashion! We will clad you in the flyest garbs.</h1>
+        <button>Shop Now</button>
+      </div>{' '}
+    </div>
+  );
 }
 
 export default Home;
