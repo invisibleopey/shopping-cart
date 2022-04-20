23234c1af3a4800d5ebb7b7bde06319b26e0a2fe
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-04-20 06:01:37 +0100

    Implement the page not found component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="not-found">
+      <h1>Error 404</h1>
+      <p>
+        Something went wrong, we are unable to find the page you requested. Go back to{' '}
+        <Link to="./">home</Link>
+      </p>
+    </div>
+  );
 }
 
 export default NotFound;
