42c3db08f6ef721d22ab927e1decc9aa562086c9
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-04-21 22:22:17 +0100

    Sort and implement product cards by their category

--- src/components/Jeans.js
+++ src/components/Jeans.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import data from '../assets/Catalogue.json';
+import Card from './Card';
 
 function Jeans() {
-  return <div>Jeans</div>;
+  const jeans = data.customCatalogue.filter((card) => {
+    return card.category === 'jeans';
+  });
+  return (
+    <section className="cards">
+      {jeans.map((card, index) => {
+        return <Card card={card} index={index} />;
+      })}
+    </section>
+  );
 }
 
 export default Jeans;
--- src/components/Shirts.js
+++ src/components/Shirts.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import data from '../assets/Catalogue.json';
+import Card from './Card';
 
 function Shirts() {
-  return <div>Shirts</div>;
+  const shirts = data.customCatalogue.filter((card) => {
+    return card.category === 'shirts';
+  });
+  return (
+    <section className="cards">
+      {shirts.map((card, index) => {
+        return <Card card={card} index={index} />;
+      })}
+    </section>
+  );
 }
 
 export default Shirts;
