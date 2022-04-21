ec27ab2bb70a37fbe7e242afeb51941c876d7600
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-04-21 21:11:16 +0100

    Implement card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-function Card() {
-  return <div>Card</div>;
+function Card(props) {
+  return (
+    <div className="card">
+      <img src={window.location.origin + props.card.url} alt={props.card.alt} id={props.index} />
+      <span className="product-title">{props.card.title}</span>
+      <span className="product-price">{`₦${props.card.price.toLocaleString()}`}</span>
+      <div>
+        <button>-</button>
+        <input type="number" min="1" step="1" value="1" />
+        <button>+</button>
+      </div>
+      <button className="add-to-cart">Add to Cart</button>
+    </div>
+  );
 }
 
 export default Card;
