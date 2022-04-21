c3725952d1f705be8db9d8d6d68bb10fff159094
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-04-21 21:27:43 +0100

    Map over all the catalogue items

--- src/components/Catalogue.js
+++ src/components/Catalogue.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import data from '../assets/Catalogue.json';
+import Card from './Card';
 
 function Catalogue() {
-  return <div>Catalogue</div>;
+  return (
+    <section className="cards">
+      {data.customCatalogue.map((card, index) => {
+        return <Card card={card} index={index} />;
+      })}
+    </section>
+  );
 }
 
 export default Catalogue;
