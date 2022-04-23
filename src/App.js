367ecdd74f04c908ec8f760eb46d9e1fdd2c7e8b
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-04-24 00:07:49 +0100

    Restructure the implementation of the router to eliminate RouteSwitch file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Shop from './components/Shop';
+import NotFound from './components/NotFound';
+import Catalogue from './components/Catalogue';
+import Shirts from './components/Shirts';
+import Jeans from './components/Jeans';
+import Sneakers from './components/Sneakers';
+import Perfume from './components/Perfume';
+import Accessories from './components/Accessories';
+import Modal from './components/Modal';
 
-function App() {
+const App = () => {
+  const location = useLocation();
+  const background = location.state && location.state.background;
+
   return (
     <div className="App">
-      <Header />
-      <Outlet />
-      <Footer />
+      <Routes location={background || location}>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/shop" element={<Shop />}>
+            <Route index element={<Catalogue />} />
+            <Route path="shirts" element={<Shirts />} />
+            <Route path="jeans" element={<Jeans />} />
+            <Route path="sneakers" element={<Sneakers />} />
+            <Route path="perfume" element={<Perfume />} />
+            <Route path="accessories" element={<Accessories />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+
+      {background && (
+        <Routes>
+          <Route path="/modal" element={<Modal />} />
+        </Routes>
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.scss';
-import RouteSwitch from './RouteSwitch';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <RouteSwitch />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
