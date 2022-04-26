bb4629c8101e7eef82af2451c2285cdb957dd2dc
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-04-27 00:08:21 +0100

    Instantiate the cart state, make it available globaly

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, createContext, useMemo } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
@@ -10,33 +11,50 @@ import Sneakers from './components/Sneakers';
 import Perfume from './components/Perfume';
 import Accessories from './components/Accessories';
 import Modal from './components/Modal';
+import data from './assets/Catalogue.json';
+
+export const CartContext = createContext();
 
 const App = () => {
   const location = useLocation();
   const background = location.state && location.state.background;
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (event, count) => {
+    const targetItem = data.customCatalogue.filter((card) => {
+      return card.id === Number(event.target.parentNode.id);
+    });
+    // Check if the item already exist, if yet, increament the count with the reccent count.
+    const newItem = [targetItem[0], count];
+    setCart((prev) => [...prev, newItem]);
+  };
+
+  const contextValue = useMemo(() => ({ cart, addToCart }), [cart]);
 
   return (
     <div className="App">
-      <Routes location={background || location}>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/shop" element={<Shop />}>
-            <Route index element={<Catalogue />} />
-            <Route path="shirts" element={<Shirts />} />
-            <Route path="jeans" element={<Jeans />} />
-            <Route path="sneakers" element={<Sneakers />} />
-            <Route path="perfume" element={<Perfume />} />
-            <Route path="accessories" element={<Accessories />} />
+      <CartContext.Provider value={contextValue}>
+        <Routes location={background || location}>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/shop" element={<Shop />}>
+              <Route index element={<Catalogue />} />
+              <Route path="shirts" element={<Shirts />} />
+              <Route path="jeans" element={<Jeans />} />
+              <Route path="sneakers" element={<Sneakers />} />
+              <Route path="perfume" element={<Perfume />} />
+              <Route path="accessories" element={<Accessories />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
           </Route>
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-
-      {background && (
-        <Routes>
-          <Route path="/modal" element={<Modal />} />
         </Routes>
-      )}
+
+        {background && (
+          <Routes>
+            <Route path="/modal" element={<Modal />} />
+          </Routes>
+        )}
+      </CartContext.Provider>
     </div>
   );
 };
