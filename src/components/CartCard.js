06e0e76783de9f7e5cbe49c695c9fa198cea9a05
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-04-27 00:09:39 +0100

    Implement a cart card to be created dynamically from cart state

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-function CartCard() {
-  return <div>CartCard</div>;
+function CartCard(props) {
+  return (
+    <div className="cart-item">
+      <img src={window.location.origin + props.card.url} alt={props.card.alt} />
+      <span className="cart-item-title">{props.card.title}</span>
+      <span className="cart-item-price">{`₦${props.card.price.toLocaleString()}`}</span>
+      {/* Calculate the real price from indicidual price x count */}
+      <div>
+        {/* Implement the functionalities of these buttons */}
+        <button>-</button>
+        <span>{props.count}</span>
+        <button>+</button>
+      </div>
+      <button className="cart-item-delete-btn">X</button>
+    </div>
+  );
 }
 
 export default CartCard;
