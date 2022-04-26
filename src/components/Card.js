82f18221b21ab8c31b10b623c06938879da8a38f
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-04-26 12:47:02 +0100

    Instantiate count state to handle the amount of each product to add to cart

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Card(props) {
+  const [count, setCount] = useState(1);
+
+  const increment = () => {
+    setCount((prevCount) => {
+      return prevCount + 1;
+    });
+  };
+
+  const decrement = () => {
+    if (!(count <= 1))
+      setCount((prevCount) => {
+        return prevCount - 1;
+      });
+  };
+
+  const inputCount = (event) => {
+    setCount(Number(event.target.value));
+  };
+
   return (
-    <div className="card">
-      <img src={window.location.origin + props.card.url} alt={props.card.alt} id={props.index} />
+    <div className="card" id={props.id}>
+      <img src={window.location.origin + props.card.url} alt={props.card.alt} />
       <span className="product-title">{props.card.title}</span>
       <span className="product-price">{`₦${props.card.price.toLocaleString()}`}</span>
       <div>
-        <button>-</button>
-        <input type="number" min="1" step="1" value="1" />
-        <button>+</button>
+        <button onClick={decrement}>-</button>
+        <input type="number" min="1" step="1" value={count} onChange={inputCount} />
+        <button onClick={increment}>+</button>
       </div>
       <button className="add-to-cart">Add to Cart</button>
     </div>
