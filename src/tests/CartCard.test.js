351fbbccfac5c0f2dd447b9ba6a56092454bd6ac
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-05-05 18:22:31 +0100

    Test the correct calculation of item price

--- src/tests/CartCard.test.js
+++ src/tests/CartCard.test.js
@@ -34,4 +34,15 @@ describe('Cart Card Component', () => {
     const image = screen.getByAltText('black and white bucket hat');
     expect(image).toHaveAttribute('src', 'http://localhost/catalogue/bucket-hat.jpg');
   });
+
+  it('renders the correct price', () => {
+    render(
+      <CartContext.Provider value={{ addToCart }}>
+        <CartCard card={card} count={count} />
+      </CartContext.Provider>,
+    );
+
+    const price = screen.getByText(/8,697/i);
+    expect(price).toBeInTheDocument();
+  });
 });
